87fb0accbfd363fa5a5f6b4713312a49f5a7698b
Eddie Herbert <abeh78@gmail.com>
2024-02-23 21:26:22 -0500

    add example

---
+++ example/main.tf
@@ -0,0 +1,30 @@
+module "superset" {
+  source = "../"
+
+  vpc_id         = "vpc-02e8793f8723abd03"
+  alb_subnet_ids = ["subnet-026f47fe8065fbc83", "subnet-0a41bdca14917cdcc", "subnet-087c22f88ff4a8db1"]
+  app_subnet_ids = ["subnet-026f47fe8065fbc83", "subnet-0a41bdca14917cdcc", "subnet-087c22f88ff4a8db1"]
+  db_subnet_ids  = ["subnet-0675d62325cac89b5", "subnet-06b815f5e7645895b", "subnet-043e5d4c2cbca1117"]
+
+  assign_public_ip = true
+
+  route53_zone   = "aws.managedoss.io"
+  route53_domain = "superset"
+
+  acm_certificate_arn = "arn:aws:acm:us-east-1:590183739501:certificate/a975e5f0-189c-4b60-be58-1c0e68084380"
+
+  image_version = "v0.1.4"
+
+  auth0_config = {
+    client_id     = "/superset-example/auth0/client_id"
+    client_secret = "/superset-example/auth0/client_secret"
+    domain        = "/superset-example/auth0/domain"
+  }
+
+  log_level = "DEBUG"
+}
+
+provider "aws" {
+  profile = "admin"
+  region  = "us-east-1"
+}
